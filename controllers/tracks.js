f6a999a0cc7305c0890ca075621f740ec4d7bd78
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-08-31 11:44:15 -0700

    updating controllers

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -25,15 +25,61 @@ router.get('/new', function(req, res) {
 	});
 });
 
-// Route for getting specific track
+// Route for getting specific track 
+router.get('/:id', function(req, res) {
+	db.Track.findOne({
+		where: { id: req.params.id },
+		include: [db.Track, db.comment, db.tag]
+	}).then(function(foundTrack) {
+		db.user.findAll().then(function(allUsers) {
+			res.render('tracks/show', { track: foundTrack, users: allUsers });
+		}).catch(function(err) {
+			console.log(err);
+			res.render('error');
+		});
+	}).catch(function(err) {
+		console.log(err);
+		res.send('failed');
+	});
+});
 
 // Route for posting an audio file / post
 router.post('/', function(req, res) {
 	if(req.body.userId > 0) {
 		db.Track.create(req.body).then(function(createdTrack) {
-			
-		})
+			// Parse tags (if there are any)
+			var tags = [];
+			if(req.body.tags) {
+				tags = req.body.tags.split(',');
+			}
+
+			if(tags.length > 0) {
+				// Loop through the tags, create one if needed, then add relation in join table
+				async.forEach(tags, function(t, done) {
+					// This code runs for each individual tag we need to add
+					db.tag.findOrCreate({
+						where: { name: t.trim() }
+					}).spread(function(newTag, wasCreated) {
+						createdTrack.addTag(newTag).then(function() {
+							done();
+						});
+					});
+				}, function() {
+					// This code runs if everything passes
+					res.redirect('/tracks/' + createdTrack.id);
+				});
+			} 
+			else {
+				res.redirect('/tracks/', + createdTrack.id);
+			}
+		}).catch(function(err) {
+			console.log(err);
+			res.render('error', { err: err });
+		});
 	}
-})
+	else {
+		res.redirect('/tracks/new')
+	}
+});
 
 module.exports = router;
