715371c1a668f2647da39d67a1d500dab6bb818f
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-09-03 10:12:53 -0700

    updated comment on track show.ejs and comment.js route in controllers.

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,7 +4,7 @@ var db = require('../models');
 
 router.post('/', function(req, res) {
 	db.comment.create(req.body).then(function(createdComment) {
-		res.redirect('/tracks/' + req.body.TrackId);
+		res.redirect('/tracks/' + req.body.trackId);
 	}).catch(function(err) {
 		res.render('error');
 	});
